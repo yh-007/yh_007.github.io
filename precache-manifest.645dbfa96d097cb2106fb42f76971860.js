f96aa9925a43412f27209659be31c0c92c045266
Yahia hachemi <56575347+yh-007@users.noreply.github.com>
2022-01-21 02:45:52 +0100

    Add files via upload

---
+++ precache-manifest.645dbfa96d097cb2106fb42f76971860.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc71c1b5e0c85e8edd7f7b46b6ce51e3",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e6e37df8f474851bfa30",
+    "url": "/home/static/css/main.eff28ab7.chunk.css"
+  },
+  {
+    "revision": "70e37b7e43cfdfe5a48d",
+    "url": "/home/static/js/2.2b93e957.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.2b93e957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e37df8f474851bfa30",
+    "url": "/home/static/js/main.2e8fc9a1.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "3c3828241320e71d0590df8f5dfce549",
+    "url": "/home/static/media/resume.3c382824.pdf"
+  },
+  {
+    "revision": "dabc4328cb2bcd1223c5ac1e871b6e1f",
+    "url": "/home/static/media/yh.dabc4328.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.645dbfa96d097cb2106fb42f76971860.js"
 );
 
 self.addEventListener('message', (event) => {
